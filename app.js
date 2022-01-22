3e67b822c6d64702c549069af48194e75e592225
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-22 19:10:43 +0530

    display alert

--- app.js
+++ app.js
@@ -43,6 +43,8 @@ function addItem(e) {
     </div>`;
     // append child
     list.appendChild(element);
+    // display alert
+    displayAlert("item added to the list", "success");
   } else if (value && editFlag) {
     console.log("editing");
   } else {
