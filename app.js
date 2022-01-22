95fb2f3b3c7c51c3e47166e4e14156abf6ce307e
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-22 17:51:39 +0530

    display alert

--- app.js
+++ app.js
@@ -29,9 +29,13 @@ function addItem(e) {
   } else if (value && editFlag) {
     console.log("editing");
   } else {
-    alert.textContent = "empty value";
-    alert.classList.add("alert-danger");
+    displayAlert("please enter value", "danger");
   }
+}
+// display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
 }
 
 // ****** LOCAL STORAGE **********
